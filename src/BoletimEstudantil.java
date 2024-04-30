328efcc01d6f51d36f7195f11264e103e2ec7a5c
= <=>
2024-04-30 13:26:42 -0300

    first commit

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        
+        int mediaFinal = 5;
+        
+/* PRINCIPIO DO IDENTAÇÃO (TAB)
+Pode ter um nivel entre 4 - 6 de identação, caso supere esse valor seria interessante
+rever o codigo para melhorar a identação e sua profundidade */
+
+        if (mediaFinal<6) 
+            System.out.println("REPROVADO");
+        else if (mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
---
+++ src/MinhaClasse.java
@@ -0,0 +1,37 @@
+public class MinhaClasse {
+
+    // main é onde sera a sua classe principal executavel
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo!");
+
+        String primeiroNome = "Robson";
+        String segundoNome = "Rocha";
+
+        // Para chamar o metodo: variavel com 'nome' e o 'valor' é a chamada do metodo
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        
+        // variaveis com a classificação final é usado quando o valor é imutavel
+        // variaavelv = tipo 'nomeVariavel' atribuição(valor)
+        final String br = "Brasil";
+        int ano = 2024;
+        final double pi = 3.14;
+        final int estadosBrasileiros = 27;
+
+        // variavel boleana tera valor sim ou não*verdadeiro ou falsa)
+        boolean maiorIdade = true;
+
+        System.out.println(nomeCompleto);
+
+    };
+
+
+    // Para declarar metodos
+    // tipoRetorno NomeObjetivoInfinitibo Parametros(opcional)
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do metodo " + primeiroNome + " " + segundoNome;
+    };
+    
+
+}
